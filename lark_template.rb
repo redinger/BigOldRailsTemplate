82aa8ccb8b8118067c322b28b90a574ace102299
Mike Gunderloy <MikeG1@larkfarm.com>
2009-07-24 06:53:13 -0500

    Move metric_fu setup to its own rakefile

--- lark_template.rb
+++ lark_template.rb
@@ -2365,18 +2365,7 @@ rake('test:parallel:prepare[4]')
 commit_state "databases set up"
 
 # rakefile for metric_fu
-file 'Rakefile', <<-END
-# Add your own tasks in files placed in lib/tasks ending in .rake,
-# for example lib/tasks/capistrano.rake, and they will automatically be available to Rake.
-
-require(File.join(File.dirname(__FILE__), 'config', 'boot'))
-
-require 'rake'
-require 'rake/testtask'
-require 'rake/rdoctask'
-
-require 'tasks/rails'
-
+rakefile 'metric_fu.rake', <<-END
 require 'metric_fu'
 MetricFu::Configuration.run do |config|
   # not doing saikuro at the moment 
