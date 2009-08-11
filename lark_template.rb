182064550a9455f6ecfcc0ce68a74f16e6857ab3
Mike Gunderloy <MikeG1@larkfarm.com>
2009-08-11 07:55:24 -0500

    Fix escaping of regex in user model

--- lark_template.rb
+++ lark_template.rb
@@ -2053,8 +2053,8 @@ END
 file 'app/models/user.rb', <<-END
 class User < ActiveRecord::Base
   acts_as_authentic do |c|
-    c.merge_validates_format_of_login_field_options :live_validator => /^\w[\w\.+\-_@ ]+$/
-    c.merge_validates_format_of_email_field_options :live_validator => "/^[A-Z0-9_\.%\+\-]+@(?:[A-Z0-9\-]+\.)+(?:[A-Z]{2,4}|museum|travel)$/i"
+    c.merge_validates_format_of_login_field_options :live_validator => /^\\w[\\w\\.+\\-_@ ]+$/
+    c.merge_validates_format_of_email_field_options :live_validator => "/^[A-Z0-9_\\.%\\+\\-]+@(?:[A-Z0-9\\-]+\\.)+(?:[A-Z]{2,4}|museum|travel)$/i"
   end
   
   serialize :roles, Array
