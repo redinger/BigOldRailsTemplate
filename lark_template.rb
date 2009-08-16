3db92d1985def1e99e7821c373f6825381b65433
Mike Gunderloy <MikeG1@larkfarm.com>
2009-08-16 06:42:59 -0500

    Update should_have_authlogic for compatibility with shoulda 2.10.2

--- lark_template.rb
+++ lark_template.rb
@@ -1361,7 +1361,7 @@ module Authlogic
   module ShouldaMacros
     class Test::Unit::TestCase
       def self.should_be_authentic
-        klass = model_class
+        klass = described_type
         should "acts as authentic" do
           assert klass.new.respond_to?(:password=)
           assert klass.new.respond_to?(:valid_password?)
