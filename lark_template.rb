850a1759249b9e857c69a2f25e1455d0ef691cf8
Mike Gunderloy <MikeG1@larkfarm.com>
2009-09-05 18:04:51 -0500

    Add git "oops" protection to deployment

--- lark_template.rb
+++ lark_template.rb
@@ -735,12 +735,14 @@ set :application, "#{current_app_name}"
 set :repository,  "git@#{capistrano_repo_host}:#{current_app_name}.git"
 set :user, "#{capistrano_user}"
 set :deploy_via, :fast_remote_cache
+set :copy_exclude, %w(.git doc test)
 set :scm, :git
 
 # Customise the deployment
 set :tag_on_deploy, false # turn off deployment tagging, we have our own tagging strategy
 
 set :keep_releases, 6
+before "deploy", "deploy:check_revision"
 after "deploy:update", "deploy:cleanup"
 
 # directories to preserve between deployments
@@ -754,6 +756,20 @@ after "deploy:update", "deploy:cleanup"
 #   end
 # end
     
+namespace :deploy do
+  desc "Make sure there is something to deploy"
+  task :check_revision, :roles => [:web] do
+    unless `git rev-parse HEAD` == `git rev-parse origin/\#{branch}`
+      puts ""
+      puts "  \033[1;33m**************************************************\033[0m"
+      puts "  \033[1;33m* WARNING: HEAD is not the same as origin/\#{branch} *\033[0m"
+      puts "  \033[1;33m**************************************************\033[0m"
+      puts ""
+ 
+      exit
+    end
+  end
+end    
 END
 
 file 'config/deploy/production.rb', <<-END
