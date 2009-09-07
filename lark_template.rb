58ffe6d9d6f302fe2a615f227af98e0da32a9764
Maxim Chernyak <max@bitsonnet.com>
2009-09-07 13:24:50 -0400

    Fix locale carryover bug.

--- lark_template.rb
+++ lark_template.rb
@@ -485,7 +485,7 @@ file 'config/database.yml', load_pattern("config/database.#{database}.yml", 'def
 file 'db/populate/01_sample_seed.rb', load_pattern('db/populate/01_sample_seed.rb')
 
 # locale
-file 'config/locales/en.yml', load_pattern('config/locale/en.yml', 'default', binding)
+file 'config/locales/en.yml', load_pattern('config/locales/en.yml', 'default', binding)
 
 commit_state "configuration files"
 
