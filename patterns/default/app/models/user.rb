b4d2afcb9d47ecaa2732f7bea7da0d5b19f57c1c
Maxim Chernyak <max@bitsonnet.com>
2009-09-15 09:37:28 -0400

    Cleanup new user validations. 4 empty fields shouldn't produce 6 validation errors.

--- patterns/default/app/models/user.rb
+++ patterns/default/app/models/user.rb
@@ -1,5 +1,10 @@
 class User < ActiveRecord::Base
   acts_as_authentic do |c|
+    config.merge_validates_format_of_login_field_options :allow_blank => true
+    config.merge_validates_format_of_email_field_options :allow_blank => true, :unless => "email.size < 6"
+    config.merge_validates_confirmation_of_password_field_options :allow_blank => true
+    config.merge_validates_length_of_password_confirmation_field_options :if => "false"
+    
     # Put any authlogic customizations here; see the authlogic help
   end
   
