4736ec0d028f49024a8c5967ba0b9d93307916df
Joey Geiger <jgeiger@jgeiger-macbook.local>
2009-09-16 14:35:43 -0500

    if mocha isn't the default, we shouldn't be requiring it anywhere by default

--- patterns/default/test/test_helper.rb
+++ patterns/default/test/test_helper.rb
@@ -8,7 +8,6 @@ end
 require File.expand_path(File.dirname(__FILE__) + "/../config/environment")
 require 'test_help'
 require 'shoulda'
-require 'mocha'
 require 'authlogic/test_case'
 require 'webrat'
 
